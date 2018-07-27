14834692f90eddd949e49d9599b7234f6bfccf1e
nagireddy146 <nagireddy9603@gmail.com>
2018-07-27 15:56:10 +0530

    third version

--- main.js
+++ main.js
@@ -12,4 +12,37 @@ function loadjson(file,callback) {
 loadjson("data.json",function(text){
 	let data=JSON.parse(text);
 	console.log(data);
+	basic(data.details);
+	edu(data.education)
 })
+var main=document.querySelector(".main")
+var left=document.createElement("div");
+left.classList.add("left");
+main.appendChild(left);
+function basic(MIC){
+var image=document.createElement("img");
+image.src=MIC.image;
+left.appendChild(image);
+var name=document.createElement("h2");
+name.textContent=MIC.name;
+left.appendChild(name);
+var email=document.createElement("h2");
+email.textContent=MIC.email;
+left.appendChild(email);
+var phone=document.createElement("h2");
+phone.textContent=MIC.phone;
+left.appendChild(phone);
+}
+var right=document.createElement("div");
+right.classList.add("right");
+main.appendChild(right);
+function edu(education)
+{
+	var un=document.createElement("ul");
+	right.appendChild(un);
+	for (var i=0;i<education.length;i++) {
+		var l=document.createElement("li");
+		l.textContent=education[i].course;
+		un.appendChild(l);
+}
+}
